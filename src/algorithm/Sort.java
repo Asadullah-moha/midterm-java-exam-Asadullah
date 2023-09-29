51ac1f1efdf5fc833c9b9f3d04ad166975d71a4c
Asadullah.Moham <Asadullah.moham@gamil.com>
2023-09-29 04:42:53 -0400

    ConnectToSqlDB

--- src/algorithm/Sort.java
+++ src/algorithm/Sort.java
@@ -35,6 +35,15 @@ public class Sort {
         final long startTime = System.currentTimeMillis();
         int [] list = array;
         //implement here
+        for (int i = 1; i < array.length; i++) {
+            int current = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > current) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = current;
+        }
 
 
 
@@ -47,9 +56,21 @@ public class Sort {
     public int[] bubbleSort(int [] array){
         int [] list = array;
         //implement here
+        boolean swapped;
 
-        
-        
+        do {
+            swapped = false;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+
         return list;
     }
     
@@ -57,8 +78,35 @@ public class Sort {
     public int [] mergeSort(int [] array){
         int [] list = array;
         //implement here
-        
-        
+        if (array.length <= 1) {
+            return array;
+        }
+        int mid = array.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[array.length - mid];
+
+        System.arraycopy(array, 0, left, 0, left.length);
+        System.arraycopy(array, left.length, right, 0, right.length);
+
+        left = mergeSort(left);
+        right = mergeSort(right);
+
+        return merge(left, right);
+    }
+
+    private int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
+        int leftIndex = 0, rightIndex = 0, resultIndex = 0;
+
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex] <= right[rightIndex]) {
+                result[resultIndex++] = left[leftIndex++];
+            } else {
+                result[resultIndex++] = right[rightIndex++];
+            }
+        }
+        System.arraycopy(left, leftIndex, result, resultIndex, left.length - leftIndex);
+        System.arraycopy(right, rightIndex, result, resultIndex, right.length - rightIndex);
 
         return list;
     }
@@ -67,8 +115,35 @@ public class Sort {
     public int [] quickSort(int [] array){
         int [] list = array;
         //implement here
-        
-        
+        return quickSort(array, 0, array.length - 1);
+    }
+
+    private int[] quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(array, low, high);
+
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+        return array;
+    }
+
+    private int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (array[j] < pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
 
         return list;
     }
@@ -76,8 +151,18 @@ public class Sort {
     public int [] heapSort(int [] array){
         int [] list = array;
         //implement here
-        
-        
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heaper(array, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            // Call heapify on the reduced heap
+            heaper(array, i, 0);
+        }
 
         return list;
     }
@@ -86,7 +171,33 @@ public class Sort {
     public int [] bucketSort(int [] array){
         int [] list = array;
         //implement here
-        
+        if (array == null || array.length == 0) {
+            return array;
+        }
+
+        int min = array[0];
+        int max = array[0];
+        for (int num : array) {
+            if (num < min) {
+                min = num;
+            } else if (num > max) {
+                max = num;
+            }
+        }
+
+        int bucketCount = max - min + 1;
+        int[] buckets = new int[bucketCount];
+        for (int num : array) {
+            buckets[num - min]++;
+        }
+
+        int index = 0;
+        for (int i = 0; i < bucketCount; i++) {
+            while (buckets[i] > 0) {
+                array[index++] = i + min;
+                buckets[i]--;
+            }
+        }
         
 
         return list;
@@ -95,8 +206,20 @@ public class Sort {
     public int [] shellSort(int [] array){
         int [] list = array;
         //implement here
-        
-        
+        int n = array.length;
+
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                int temp = array[i];
+                int j;
+
+                for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+                    array[j] = array[j - gap];
+                }
+
+                array[j] = temp;
+            }
+        }
 
         return list;
     }
